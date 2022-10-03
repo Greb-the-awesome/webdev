8bc82053413e5da371ad5de4abb9644b7493e703
Greb <gyourselfolden@gmail.com>
2022-10-02 21:29:31 -0400

    heroku issues #3 xd

--- main.py
+++ main.py
@@ -109,5 +109,5 @@ def handleMsg(texts):
 @app.errorhandler(404)
 def handle_404(e):
 	return '<center><h1>Oh No!</h1><br><p>An error 404 occured.</p></center>'
-
-socketio.run(app)
+if __name__ == "__main__":
+	socketio.run(app)
